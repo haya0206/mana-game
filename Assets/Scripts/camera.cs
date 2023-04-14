7fbd8c6a6df18cbf8a7ce5332ec75f009d5d828a
KimYoungHa <haya0206333@gmail.com>
2023-04-14 15:56:55 +0900

    Add camera script

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    public float maxX = 10f;
+    public float minX = -10f;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x, minX, maxX), transform.position.y, transform.position.z);
+    }
+}
